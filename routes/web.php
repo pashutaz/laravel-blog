1dc1b5bbdf93a2ef0d7ee9f7dfa3d8e5d2028996
Pavel Zaytsev <pazaytsev@gmail.com>
2018-10-05 18:33:20 +0300

    update

--- routes/web.php
+++ routes/web.php
@@ -1,37 +1,3 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/tasks', 'TasksController@index');
-Route::get('/tasks/{task}', 'TasksController@show');
-
-
-//Route::get('/tasks', function () {
-////	$tasks = DB::table('tasks')->get(); //query builder
-////    return $tasks;
-//
-//    $tasks = \App\Task::all();
-//	return view('tasks.index', compact('tasks'));
-//});
-//
-//Route::get('/tasks/{task_id}', function ($id) {
-//	// dd($id); //debug
-////	$task = DB::table('tasks')->find($id);
-////	return $task;
-//
-//    $task = \App\Task::find($id);
-//	return view('tasks.show', compact('task'));
-//});
+Route::get('/', 'PostController@index');
